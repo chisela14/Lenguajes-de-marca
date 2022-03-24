052c5eed7c621d9a521bcc939e294cb4aeac1589
chisela14 <chisela14@gmail.com>
2022-03-24 12:31:26 +0100

    inicio javaScript

---
+++ JavaScript/scripts.js
@@ -0,0 +1,44 @@
+/*
+//1. Básico
+let numero = -4;
+let palabra = "Hola Mundo";
+let respuesta = true;
+const PI = 3.14;
+numero = 54;
+numero2 = 34; //el ide te dice algo pero se declara sin let
+console.log(numero, palabra, respuesta, PI, numero2);
+//PI = 3.1415; error invalid assigment
+*/
+/*
+//2. Operadores aritméticos
+let var1 = 5, var2 = 2;   
+console.log (var1 + var2);
+console.log (var1 - var2);
+console.log (var1 * var2);
+console.log (var1 / var2);
+console.log (var1 % var2);
+let nombre = "Chisela";
+console.log ("Hola " + nombre);
+*/
+/*
+//3. Incremento/Decremento
+let a = 5;
+console.log(a); //5
+console.log(a++); //5 (muestras a e incrementa)
+console.log(a); //6
+console.log(++a); //7 (incrementa y muestras a)
+let b = 5;
+console.log(b); //5
+console.log(b--); //5
+console.log(b); //4
+console.log(--b); //3
+console.log(a+=3);
+console.log(a-3);
+*/
+
+//4. Strings
+let cadena = "Hola Mundo";
+console.log (cadena.length);
+console.log (cadena.toUpperCase());
+console.log(cadena.toLowerCase());
+console.log(cadena.indexOf('o'));
